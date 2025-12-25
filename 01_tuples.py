aa16a9fcae2f964256b59369d27f0e0173b4dfda
kajal-rajgurav <rajguravkajal123@gmail.com>
2025-12-25 19:46:45 +0530

    git hub tutorial

---
+++ 01_tuples.py
@@ -0,0 +1,22 @@
+# the tuple is the collection of homogenous or heterogenous data.
+# enclosed between Parenthesis ().
+
+"""syntax: 
+        var_name = (val1, val2, val3, ..., valn)
+                        or
+        var_name = val1, val2, val3, ..., valn
+                        or
+        var_name = (val1) # to initialize single element in tuple.
+
+"""
+
+
+# Eg 1.
+numbers = (1, 3, 4, 5)
+print(numbers)
+
+first=numbers[1]
+print(first)
+
+
+
