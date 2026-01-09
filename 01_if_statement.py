18aac4e238c7ff00c57526459d7619c3ba19e9ad
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-09 20:57:48 +0530

    practice

---
+++ 01_if_statement.py
@@ -0,0 +1,7 @@
+signal=str(input("enter your signal:-"))
+if signal=='red':
+    print("stopp")
+elif signal=='yellow':
+    print("ready to go")
+elif signal=='green':
+    print("goooo")
---
+++ 03_patterns.py
@@ -0,0 +1,14 @@
+# *
+# * *
+# * * *
+# * * * *
+
+n=4
+for i in range(1,n+1):
+    print('*'*i)
+
+# reverse.
+n=4
+for i in range(n,0,-1):
+    print('*'*i)
+    
