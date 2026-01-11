d931df8f4880258db6f72e600006941907d290eb
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-11 18:29:47 +0530

    daily

--- 03_patterns.py
+++ 03_patterns.py
@@ -18,15 +18,30 @@
 #         print("A","B","C"*i)
 
 
-n=4
-for i in range(1,n+1):
-        print(" "*(n-i)+"* "* i)
-for i in range(n-1,0,-1):
-     print(" "*(n-i) + "* " * i)
+# n=4
+# for i in range(1,n+1):
+#         print(" "*(n-i)+"* "* i)
+# for i in range(n-1,0,-1):
+#      print(" "*(n-i) + "* " * i)
 
-n = 4
-for i in range(1, n+1):
-    print(" " * (n-i) + "* " * i)
+# n = 4
+# for i in range(1, n+1):
+#     print(" " * (n-i) + "* " * i)
 
-for i in range(n-1, 0, -1):
-    print(" " * (n-i) + "* " * i)
+# for i in range(n-1, 0, -1):
+#     print(" " * (n-i) + "* " * i)
+
+
+# sqaure pattern.
+#  *  *  *  * 
+#  *  *  *  * 
+#  *  *  *  * 
+#  *  *  *  * 
+#  *  *  *  * 
+# n=4
+# for i in range(1,6):
+#      print(" * "*n)
+
+# triangle
+for i in range(1,6):
+    print(" * "*i,end="")
